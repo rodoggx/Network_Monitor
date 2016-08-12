b3f8f09e95d714371a1d44e0367c5cfce2d6bcfd
rodoggx <mikerodolph@yahoo.com>
2016-08-11 20:01:46 -0400

    add snackbar

--- app/src/main/java/com/example/networkmonitor/MainActivity.java
+++ app/src/main/java/com/example/networkmonitor/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.networkmonitor;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "doMagicTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void doMagic(View view) {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        if (activeNetwork != null) {
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                Snackbar.make(view, "Connected to WiFi", Snackbar.LENGTH_LONG)
+                        .show();
+
+                Log.d(TAG, "checkInternet: " + "Connected to WIFI");
+            } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+
+                Log.d(TAG, "checkInternet: " + "Connected to Mobile data");
+            }
+        } else {
+            Snackbar.make(view, "Not connected to WiFi", Snackbar.LENGTH_LONG)
+                    .show();
+            Log.d(TAG, "checkInternet: " + "Not connected");
+        } }
 }
